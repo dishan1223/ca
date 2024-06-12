a60c05d046b0da74f5014366a254560c2e043284
dishan1223 <dishanishtiaq45+@gmail.com>
2024-06-12 12:11:23 +0600

    added an uninstall.sh file to uninstall the program from systemw

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# instructions to uninstall
+# 1. chmod +x uninstall.sh
+# 2. ./uninstall.sh
+
+
+# remove the script from bin file 
+# that will technically uninstall the program from you system
+sudo rm /usr/local/bin/ca
